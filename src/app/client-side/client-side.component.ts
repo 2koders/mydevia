ca4fff87d415bfb90fc557e4df263e27b6ca9839
2koders <2koders@gmail.com>
2020-08-29 13:44:35 -0400

    Logos de clientes agregandose

--- src/app/client-side/client-side.component.ts
+++ src/app/client-side/client-side.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-client-side',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-side.component.css']
 })
 export class ClientSideComponent implements OnInit {
+
+  map = faMapMarkedAlt;
 
   constructor() { }
 
