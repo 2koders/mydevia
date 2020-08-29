50a1ef7bacbedbec054fdd722ef05b3de4550308
2koders <2koders@gmail.com>
2020-08-29 02:20:39 -0400

    waiting page y organicé el directorio

--- src/app/_pages/contactos/contactos.component.ts
+++ src/app/_pages/contactos/contactos.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-contactos',
   templateUrl: './contactos.component.html',
   styleUrls: ['./contactos.component.css']
 })
--- src/app/_pages/maquina/maquina.component.ts
+++ src/app/_pages/maquina/maquina.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-maquina',
   templateUrl: './maquina.component.html',
   styleUrls: ['./maquina.component.css']
 })
